ed5d305e38416fc80c278e6394a3ff320ab18212
Ysnezko <109404402+Ysnezko@users.noreply.github.com>
2022-07-16 10:38:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ Tol = await getBuffer(`https://hardianto.xyz/api/goodbye3?profile=${encodeURICom
                 } else if (anu.action == 'remove') {
                     GojoMdNx.sendMessage(anu.id, { image: Tol, contextInfo: { mentionedJid: [num] }, caption: `⭐✑ @${num.split("@")[0]} Left ${metadata.subject}
 
-⭐✑ I'm Not Sure If It Was A Goodbye Charm, But It Was Fun While It Lasted 😌✨` })
+⭐✑ I'm Not Sure If It Was A Goodbye Charm, Apo Sheri Da Gud Bye😌✨` })
                 }
             }
         } catch (err) {
